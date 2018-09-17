e5a91d2c85cfebc358c0d6ab60b5e7715fb261ab
Heiner Pöpping <heiner.poepping@t-online.de>
2018-09-17 11:54:18 +0200

    Added DataAnnotations to Employee

--- CompanyASP.NET/Models/Employee.cs
+++ CompanyASP.NET/Models/Employee.cs
@@ -1,19 +1,25 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
 
 namespace CompanyASP.NET.Models
 {
     public class Employee : IModel
     {
+        [Key]
         public int? Id { get; set; }
+        [Editable(false)]
         public int? PersonId { get; set; }
+        [Required]
         public string LastName { get; set; }
+        [Required]
         public string FirstName { get; set; }
+        [Required]
         public DateTime? Birthday { get; set; }
+        [Required]
         public string Phone { get; set; }
+        [Required]
         public string Gender { get; set; }
+        [Required]
         public DateTime? EmployeeSince { get; set; }
         public bool Identity(int[] ids)
         {
